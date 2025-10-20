9772037e56ece2920024bef04bf540ce9c6c9b00
ririyeye <200610237@qq.com>
2025-10-20 22:54:03 +0800

    fix: 更新依赖关系，统一使用 co_syswork_common 作为依赖项

--- test/xmake.lua
+++ test/xmake.lua
@@ -1,36 +1,38 @@
+target("co_syswork_common")
+set_kind("object")
+add_deps("co_wq")
+add_files("co_syswork.cpp")
+target_end()
+
 target("co_echo")
 set_kind("binary")
-add_deps("co_wq")
+add_deps("co_syswork_common")
 
 add_files("co_echo.cpp")
-add_files("co_syswork.cpp")
 
 target_end()
 
 target("co_chat")
 set_kind("binary")
-add_deps("co_wq")
+add_deps("co_syswork_common")
 add_files("co_chat_server.cpp")
-add_files("co_syswork.cpp")
 add_packages("nlohmann_json")
 target_end()
 
 target("co_http")
 set_kind("binary")
-add_deps("co_wq")
+add_deps("co_syswork_common")
 
 add_files("co_http_server.cpp")
-add_files("co_syswork.cpp")
 add_packages("nlohmann_json")
 
 target_end()
 
 target("co_curl")
 set_kind("binary")
-add_deps("co_wq")
+add_deps("co_syswork_common")
 
 add_files("co_curl.cpp")
-add_files("co_syswork.cpp")
 
 target_end()
 
@@ -48,64 +50,57 @@ target_end()
 
 target("co_http_proxy")
 set_kind("binary")
-add_deps("co_wq")
+add_deps("co_syswork_common")
 
 add_files("co_http_proxy.cpp")
-add_files("co_syswork.cpp")
 
 target_end()
 
 target("co_socks_proxy")
 set_kind("binary")
-add_deps("co_wq")
+add_deps("co_syswork_common")
 
 add_files("co_socks_proxy.cpp")
-add_files("co_syswork.cpp")
 
 target_end()
 
 target("co_ws")
 set_kind("binary")
-add_deps("co_wq")
+add_deps("co_syswork_common")
 
 add_files("co_websocket_server.cpp")
-add_files("co_syswork.cpp")
 
 target_end()
 
 target("co_cp")
 set_kind("binary")
-add_deps("co_wq")
-add_files("co_syswork.cpp")
+add_deps("co_syswork_common")
 add_files("co_cp.cpp")
 target_end()
 
 if has_config("USING_USB") then
     target("co_usb")
     set_kind("binary")
-    add_deps("co_wq")
+    add_deps("co_syswork_common")
     add_files("co_usb.cpp")
-    add_files("co_syswork.cpp")
     target_end()
 end
 
 if is_plat("linux") then
     target("co_uds")
     set_kind("binary")
-    add_deps("co_wq")
+    add_deps("co_syswork_common")
     add_files("co_uds.cpp")
-    add_files("co_syswork.cpp")
     target_end()
 end
 
 if has_config("USING_SSL") then
     target("co_bili")
     set_kind("binary")
-    add_deps("co_wq")
+    add_deps("co_syswork_common")
     add_rules("utils.bin2c", { linewidth = 16, extensions = { ".html" } })
     add_files("static/bili_index.html")
     add_files("co_bilibili_ranking.cpp")
-    add_files("co_syswork.cpp")
     add_packages("nlohmann_json")
     target_end()
 end
